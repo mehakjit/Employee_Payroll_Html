8f1abbc7b1af627bb4404ce54c047bdceab9a68a
mehakjit <mehakjitsingh@gmail.com>
2020-11-20 07:45:58 +0530

    [Mehakjit] Added: Validation for Name and start date

--- js/payroll.js
+++ js/payroll.js
@@ -4,6 +4,27 @@ salary.addEventListener('input',function(){
     output.textContent = salary.value;
 }); 
 
+const name = document.querySelector("#name");
+const textError = document.querySelector(".text-error");
+const nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
+name.addEventListener("input", function () {
+  if (nameRegex.test(name.value)) textError.textContent = "";
+  else textError.textContent = "Name is Incorrect!";
+});
+
+const date = document.querySelector(".date");
+const dateError = document.querySelector(".date-error");
+date.addEventListener("input", function () {
+    var day = document.getElementById("day").value;
+    var month = document.getElementById("month").value;
+    var year = document.getElementById("year").value;
+    var startDate = new Date(parseInt(year), parseInt(month) - 1, parseInt(day));
+    let days = 30;
+    var todayDate = new Date();
+    var lastDate = new Date(todayDate.getTime() - (days * 24 * 60 * 60 * 1000));
+    if (startDate>=lastDate && startDate<=todayDate) dateError.textContent = "";
+    else dateError.textContent = "Enter valid date!";
+});
 
 function save(){
     var name= document.getElementById("name").value;
