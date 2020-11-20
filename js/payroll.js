0461dcecc449a3a6e35bbba78973b06bf7ca2d4e
mehakjit <mehakjitsingh@gmail.com>
2020-11-20 07:52:13 +0530

    [Mehakjit] Added: Ability to create and save payroll obj

--- js/payroll.js
+++ js/payroll.js
@@ -1,43 +1,103 @@
-const salary = document.querySelector('#salary');
-const output = document.querySelector('.salary-output');
-salary.addEventListener('input',function(){
-    output.textContent = salary.value;
-}); 
+window.addEventListener('DOMContentLoaded', (event)=>{
+    const name = document.querySelector("#name");
+    const textError = document.querySelector(".text-error");
+    name.addEventListener("input", function () {
+        if (name.value.length==0){
+            textError.textContent = "";
+            return;
+        }
+        try{
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent = "";
+        }catch(e){
+            textError.textContent = e;
+        }
+    });
 
-const name = document.querySelector("#name");
-const textError = document.querySelector(".text-error");
-const nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
-name.addEventListener("input", function () {
-  if (nameRegex.test(name.value)) textError.textContent = "";
-  else textError.textContent = "Name is Incorrect!";
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    salary.addEventListener('input',function(){
+        output.textContent = salary.value;
+    }); 
+    
+    const date = document.querySelector(".date");
+    const dateError = document.querySelector(".date-error");
+    date.addEventListener("input", function () {
+        var day = document.getElementById("day").value;
+        var month = document.getElementById("month").value;
+        var year = document.getElementById("year").value;
+        var startDate = new Date(parseInt(year), parseInt(month) - 1, parseInt(day));
+        try{
+            (new EmployeePayrollData()).startDate = startDate;
+            dateError.textContent = "";
+        }catch(e){
+            dateError.textContent = e;
+        }
+    });
+    
 });
 
-const date = document.querySelector(".date");
-const dateError = document.querySelector(".date-error");
-date.addEventListener("input", function () {
-    var day = document.getElementById("day").value;
-    var month = document.getElementById("month").value;
-    var year = document.getElementById("year").value;
-    var startDate = new Date(parseInt(year), parseInt(month) - 1, parseInt(day));
-    let days = 30;
-    var todayDate = new Date();
-    var lastDate = new Date(todayDate.getTime() - (days * 24 * 60 * 60 * 1000));
-    if (startDate>=lastDate && startDate<=todayDate) dateError.textContent = "";
-    else dateError.textContent = "Enter valid date!";
-});
+const save = ()=>{
+    try{
+        let employeePayrollData = createEmployeePayroll();
+    }catch(e){
+        return ;
+    }
+};
 
-function save(){
-    var name= document.getElementById("name").value;
-    var picture = document.querySelector('input[name = profile]:checked').value;
-    var gender = document.querySelector('input[name = gender]:checked').value;
-    var department =document.querySelector('input[name = department]:checked').value;
-    var salary = document.getElementById("salary").value;
-    var day = document.getElementById("day").value;
-    var month = document.getElementById("month").value;
-    var year = document.getElementById("year").value;
-    var note = document.getElementById("notes").value;
-    var startDate = new Date(parseInt(year), parseInt(month) - 1, parseInt(day));
+const  createEmployeePayroll = ()=>{
+    let employeePayrollData = new EmployeePayrollData();
+    try{
+        employeePayrollData.name = getInputValueById('#name');
+    }catch(e){
+        setTextValue('.text-error',e);
+        throw e;
+    }
 
-   const employeePayrollData = new EmployeePayrollData(name, picture, gender, department, salary, startDate, note);
-   alert("Entered data is saved!\n" + employeePayrollData.toString());
-  }  
+    employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+    employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+    employeePayrollData.department = getSelectedValues('[name=department]');
+    employeePayrollData.salary = getInputValueById('#salary');
+    employeePayrollData.note = getInputValueById('#notes');
+
+    let date = getInputValueById('#day')+" "+getInputValueById('#month')+" "+
+    getInputValueById('#year');
+    employeePayrollData.date = Date.parse(date);
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+}
+
+const getSelectedValues = (propertyValue)=>{
+    let allItems = document.querySelectorAll(propertyValue);
+    let selItems =[];
+    allItems.forEach(item=>{
+        if(item.checked) selItems.push(item.value);
+    });
+    return selItems;
+}
+
+const getInputValueById = (id)=>{
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+const getInputElementValue = (id)=>{
+    let value = document.getElementById(id).value;
+    return value;
+}
+
+// function save(){
+//     var name= document.getElementById("name").value;
+//     var picture = document.querySelector('input[name = profile]:checked').value;
+//     var gender = document.querySelector('input[name = gender]:checked').value;
+//     var department =document.querySelector('input[name = department]:checked').value;
+//     var salary = document.getElementById("salary").value;
+//     var day = document.getElementById("day").value;
+//     var month = document.getElementById("month").value;
+//     var year = document.getElementById("year").value;
+//     var note = document.getElementById("notes").value;
+//     var startDate = new Date(parseInt(year), parseInt(month) - 1, parseInt(day));
+
+//    const employeePayrollData = new EmployeePayrollData(name, picture, gender, department, salary, startDate, note);
+//    alert("Entered data is saved!\n" + employeePayrollData.toString());
+//   } 
